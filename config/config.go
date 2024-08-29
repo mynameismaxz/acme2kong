538701916b61d74b95a3e647da201c25dbb82b76
mynameismaxz <sirachat.sir@gmail.com>
2024-08-29 18:38:39 +0700

    feat: change configuration environment of cloudflare

--- config/config.go
+++ config/config.go
@@ -20,9 +20,11 @@ func Initialize() (*Config, error) {
 	viper.SetDefault("DOMAIN_NAME", "*.mymacz.com")
 	viper.SetDefault("REGISTRATION_EMAIL", "")
 	viper.SetDefault("CHALLENGE_TYPE", "dns")
+	// With the cloudflare-dns challenge provider, need to use the Cloudflare API to create the DNS record
+
 	viper.SetDefault("CHALLENGE_PROVIDER", "cloudflare")
-	viper.SetDefault("CLOUDFLARE_EMAIL", "")
-	viper.SetDefault("CLOUDFLARE_API_KEY", "")
+	viper.SetDefault("CF_ZONE_API_TOKEN", "")
+	viper.SetDefault("CF_DNS_API_TOKEN", "")
 
 	config := &Config{
 		KongEndpoint:      viper.GetString("KONG_ENDPOINT"),
